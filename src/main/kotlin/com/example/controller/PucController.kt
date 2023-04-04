ff106ff3d096fc3d8090f307e74eba385a87863d
unknown <heitorcarrazzoni@gmail.com>
2023-04-04 13:30:30 -0300

    hs

---
+++ src/main/kotlin/com/example/controller/PucController.kt
@@ -0,0 +1,17 @@
+package com.example.controller
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class PucController {
+
+    @RequestMapping("/api/hello", method = [RequestMethod.GET])
+    fun hello(): String {
+        return "Hello CI/CD PUC"
+    }
+}
--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,8 +3,9 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
-@EnableAutoConfiguration
+@ComponentScan("com.example")
 @SpringBootApplication
 class DemoApplication
 
